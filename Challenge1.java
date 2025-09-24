0b962d7827940e8a2d0165bdae24286b792f528c
Ritu Kumari <ritu89159@gmail.com>
2025-09-24 23:15:13 +0530

    Add files via upload

---
+++ Challenge1.java
@@ -0,0 +1,7 @@
+// Create a class to output "Good Morning" using a text editor and check output
+
+public class Challenge1{
+    public static void main(Stirng [] args){
+        system.out.println("Good Morning");
+    }
+}
