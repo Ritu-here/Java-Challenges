7014e47d97ecf03bde9e050109467fa1190af825
Ritu Kumari <ritu89159@gmail.com>
2025-09-24 23:15:49 +0530

    Add files via upload

---
+++ Challenge2.java
@@ -0,0 +1,8 @@
+//Create a new project in intellij idea and output " Subscribe" on the console
+
+
+public class Challenge2 {
+    public static void mian(String [] args){
+        System.out.println("Subscribe");
+    }
+}
