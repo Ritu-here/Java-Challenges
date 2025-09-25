ed8b7391f83030ac2740ff0ad3575f621d2014fa
Ritu Kumari <ritu89159@gmail.com>
2025-09-25 21:42:32 +0530

    Add files via upload

---
+++ Challenge4.java
@@ -0,0 +1,9 @@
+//Show the following patterns using single print statement
+
+public class Challenge4 {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****");
+        System.out.println("*****\n****\n***\n**\n*");
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
+    }
+}
