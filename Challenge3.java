40653415994c0b78c1a54379e238788d3c302280
Ritu Kumari <ritu89159@gmail.com>
2025-09-25 21:41:16 +0530

    Add files via upload

---
+++ Challenge3.java
@@ -0,0 +1,34 @@
+//Show the following pattern using only print statement
+//*                   *****
+//**                  ****
+//***                 ***
+//****                **
+//*****               *
+
+public class Challenge3 {
+    public static void main(String[] args) {
+        System.out.println("Right Half Pyramid");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+
+        System.out.println();
+        System.out.println("Reverse Right Half Pyramid");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        System.out.println();
+        System.out.println("Left Half Pyramid");
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+    }
+}
