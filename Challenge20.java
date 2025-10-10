b9b0dba71e837943e46ad79135b26a03a9d9a238
Ritu Kumari <ritu89159@gmail.com>
2025-10-10 22:24:51 +0530

    Add files via upload

---
+++ Challenge20.java
@@ -0,0 +1,24 @@
+//Create a program that categorize a person into different age groups
+//Child --> below 13 , Teen --> below 20 , Adult --> below 60 , Senior --> above 60
+
+
+import java.util.Scanner;
+public class Challenge20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age to know your category");
+        int age = input.nextInt();
+        if(age <= 13){
+            System.out.println(" You are Child");
+        } else if (age < 20) {
+            System.out.println("You are Teen ");
+
+        } else if (age > 20 & age <60) {
+            System.out.println("You are Adult");
+
+        }
+        else{
+            System.out.println("You are Senior");
+        }
+    }
+}
